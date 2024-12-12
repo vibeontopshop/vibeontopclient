58310be3188ae72d21aa756fccf8a2ed76929309
Ranit-dev2004 <154494417+Ranit-dev2004@users.noreply.github.com>
2024-12-12 12:01:22 +0530

    Update page.jsx

--- src/app/forgotpass/page.jsx
+++ src/app/forgotpass/page.jsx
@@ -42,7 +42,7 @@ export default function SignUp() {
           &lt; Back
         </Link></p>
           <h1 className="text-4xl font-bold text-gray-900">Forgot Password</h1>
-          <p className="mt-3 text-sm text-gray-800">Enter Your registered email address. we'll send you a code to reset your password</p>
+          <p className="mt-3 text-sm text-gray-800">Enter Your registered email address. we will send you a code to reset your password</p>
 
           <form className="mt-6 space-y-4">
             {/* First Name */}
